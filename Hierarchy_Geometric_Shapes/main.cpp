f143fb33a91262c2ae6466dc0980db7f69e6fb6c
Aleksey <sav459811@mail.ru>
2022-02-08 18:08:44 +0500

    done NO radius and random

--- Hierarchy_Geometric_Shapes/main.cpp
+++ Hierarchy_Geometric_Shapes/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Figure
 {
 	unsigned int length;
+	unsigned int lengthtwo;
 	unsigned int radius;
 public:
 	virtual void Square() = 0; // Абстрактное подсчет площади
@@ -14,9 +15,17 @@ public:
 	{
 		return length;
 	}
+	unsigned int get_lengthtwo()const
+	{
+		return lengthtwo;
+	}
 	void set_length(unsigned int length)
 	{
 		this->length = length;
+	}
+	void set_lengthtwo(unsigned int lengthtwo)
+	{
+		this->lengthtwo = lengthtwo;
 	}
 	//=========== get и set для изменения переменных==========
 	
@@ -25,6 +34,12 @@ public:
 	{
 		set_length(length);
 		
+	}
+
+	Figure(unsigned int length, unsigned int lengthtwo)
+	{
+		set_length(length);
+		set_lengthtwo(lengthtwo);
 	}
 		
 	~Figure() {}
@@ -111,6 +126,76 @@ public:
 
 };
 
+class Rectangle:Figure
+{
+public:
+	Rectangle(unsigned int length,unsigned int lengthtwo):Figure(length,lengthtwo) {}
+	~Rectangle(){}
+
+	void Square()
+	{ 
+		int lengthSq;
+		lengthSq = get_length() * get_lengthtwo();
+		cout << "Squre Rectangle: " << lengthSq << endl;
+	}
+
+	void Perimeter()
+	{
+		int lengthPer;
+		lengthPer = get_length() + get_lengthtwo();
+		cout << "Perimetr Rectangle: " << lengthPer << endl;
+	}
+
+	void print()
+	{
+		char Ascii[256];
+		int s;
+		s = 218;
+		Ascii[s] = s;
+		cout << Ascii[s];
+
+		s = 196;
+		Ascii[s] = s;
+		for (int i = 0; i < get_length(); i++) cout << Ascii[s];
+
+		s = 191;
+		Ascii[s] = s;
+		cout << Ascii[s] << endl;
+
+		s = 179;
+		Ascii[s] = s;
+
+		for (int l = 0; l < get_lengthtwo()/2; l++)
+		{
+			for (int j = 0; j < get_length() + 1; j++)
+			{
+				for (int i = 0; i < get_length(); i++)
+				{
+					if (j == 0) {
+						cout << Ascii[s];
+						break;
+					}
+					else { cout << " "; break; }
+					//cout << Ascii[s] << endl;
+				}
+				if (j == get_length()) cout << Ascii[s] << endl;
+			}
+		}
+
+		s = 192;
+		Ascii[s] = s;
+		cout << Ascii[s];
+
+		s = 196;
+		Ascii[s] = s;
+		for (int i = 0; i < get_length(); i++) cout << Ascii[s];
+
+		s = 217;
+		Ascii[s] = s;
+		cout << Ascii[s] << endl;
+
+	}
+};
 
 
 
@@ -118,10 +203,17 @@ public:
 void main()
 {
 	//Figure Kvadrat();
-	Area Kvadrat(19);
+	Area Kvadrat(9);
 	Kvadrat.Square();
 	Kvadrat.Perimeter();
 	Kvadrat.print();
+
+	// Figure Rectangle
+
+	Rectangle Pramougol(20, 10);
+	Pramougol.Square();
+	Pramougol.Perimeter();
+	Pramougol.print();
 
 
 }
